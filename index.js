72b1f231dcce498cb6e5c644ccddf5fa7e19be8b
RenzoOrtiz <renzoortizlbk@gmail.com>
2021-10-15 12:51:07 -0500

    Primer commit

---
+++ index.js
@@ -0,0 +1,142 @@
+const AWS = require("aws-sdk");
+const { nanoid } = require('nanoid')
+
+var dynamodb = new AWS.DynamoDB({
+    apiVersion: "2012-08-10",
+    endpoint: "http://dynamodb:8000",
+    region: "us-west-2",
+    credentials: {
+        accessKeyId: "2345",
+        secretAccessKey: "2345",
+    },
+});
+
+var docClient = new AWS.DynamoDB.DocumentClient({
+    apiVersion: "2012-08-10",
+    service: dynamodb,
+});
+
+exports.handler = async (event) => {
+
+    console.log("EVENTO: ", event);
+
+    if (event.httpMethod = "GET"){
+
+        if (event.path === "/envios/pendientes") {
+            let params = {
+                TableName: 'Envio',
+                IndexName: 'EnviosPendientesIndex'
+            };
+
+            let resp
+            await docClient.scan(params, (err, data) => {
+                if (!err) {
+                    resp = {
+                        statusCode: 200,
+                        body: (data.Items.length) ? data : 'El listado de envios pendientes está vacio',
+                    }
+                }
+                else {
+                    resp = {
+                        statusCode: 500,
+                        body: 'El listado de envios pendientes no está disponible',
+                    }
+                }
+            }).promise();
+
+            return resp;
+        }
+    }
+
+    if (event.httpMethod = "POST"){
+
+        if (event.path === '/envios') {
+            let body = JSON.parse(event.body)
+            if (!event.body || !body.email || !body.destino) {
+                console.log("BODY: ", body);
+                console.log("EMAIL: ", body.email);
+                console.log("DESTINO: ", body.destino);
+                return {
+                    statusCode: 400,
+                    body: 'La informacion enviada está incompleta'
+                }
+            }
+            let params = {
+                TableName: 'Envio',
+                Item: {
+                    id: nanoid(),
+                    fechaAlta: new Date().toLocaleString(),
+                    destino: body.destino,
+                    email: body.email,
+                    pendiente: new Date().toLocaleString()
+                }
+            };
+            let resp
+            await docClient.put(params, function (err, data) {
+                console.log("Data: ", data);
+                if (!err) {
+                    resp = {
+                        statusCode: 200,
+                        body: params.Item
+                    }
+                }
+                else {
+                    resp = {
+                        statusCode: 500,
+                        body: 'No fue posible guardar el envio'
+                    }
+                }
+            }).promise()
+
+            return resp
+        }
+    }
+
+    if (event.httpMethod = "PUT"){
+
+        if (event.path === `/envios/${event.pathParameters.idEnvio}/entregado`) {
+            const id = event.pathParameters.idEnvio
+            let params = {
+                TableName: "Envio",
+                Key: {
+                    "id": event.pathParameters.idEnvio
+                },
+                UpdateExpression: "REMOVE pendiente",
+                ReturnValues: "ALL_NEW"
+            };
+
+            let resp
+            await docClient.update(params, function (err, data) {
+                if (!err) {
+
+                    if (Object.keys(data.Attributes).length === 1) {
+                        resp = {
+                            statusCode: 200,
+                            body: `El envio con id ${id} no existe`
+                        }
+                    }
+                    else {
+                        resp = {
+                            statusCode: 200,
+                            body: data.Attributes
+                        }
+                    }
+                } else {
+                    resp = {
+                        statusCode: 500,
+                        body: 'Fallo la actualizacion del envio seleccionado'
+                    }
+                }
+            }).promise();
+
+            return resp
+        }
+        else {
+            return {
+                statusCode: 400,
+                body: 'El id o la ruta ingresada es incorrecta'
+            }
+        }
+    }
+};
+
